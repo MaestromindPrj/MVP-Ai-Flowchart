afce82d2141285da32a5493445383ec60405c291
Aswin <aswinram.r.m@maestrominds.co.in>
2026-09-23 22:25:28 +0530

    commit

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -4,12 +4,24 @@ declare global {
   var prismaGlobal: PrismaClient | undefined;
 }
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = "file:./dev.db";
+}
+
 export const prisma =
   globalThis.prismaGlobal ??
   new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
     log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prismaGlobal = prisma;
 }
+
+
+
